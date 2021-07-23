dcc27fdd7ce5f8b0dea99b3f8281d67700933477
Mario Inga <mario21ic@gmail.com>
2021-07-23 14:48:08 -0500

    feat(global) adding packer example

---
+++ 3-packer/README.md
@@ -0,0 +1,11 @@
+Dependencies installation:
+```
+go mod init "github.com/mario21ic/terratest-demos"
+go get -u -v -f all
+```
+
+Running tests:
+```
+go test -v
+```
+
---
+++ 3-packer/packer_hello_world_example_test.go
@@ -0,0 +1,22 @@
+package test
+
+import (
+    "testing"
+
+    "github.com/gruntwork-io/terratest/modules/docker"
+    "github.com/gruntwork-io/terratest/modules/packer"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestPackerHelloWorldExample(t *testing.T) {
+    packerOptions := &packer.Options{
+        Template: "./build.json",
+    }
+
+    packer.BuildArtifact(t, packerOptions)
+
+    opts := &docker.RunOptions{Command: []string{"cat", "/test.txt"}}
+    output := docker.Run(t, "gruntwork/packer-hello-world-example", opts)
+    assert.Equal(t, "Hello, World!", output)
+}
+
